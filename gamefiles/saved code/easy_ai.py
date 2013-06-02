0eb30d49b8a1a473887baff386349a01577419f6
boomer <herman.rogers@gmail.com>
2013-06-02 23:19:35 +0100

    Adding a stronger modular structure while implementing tests to check various class functions

---
+++ gamefiles/saved code/easy_ai.py
@@ -0,0 +1,16 @@
+#def randomComputerAI(all_positions):
+    '''This is for an easy AI option, left in for future implementations'''
+
+#    posx, posy = random.randint(0,2), random.randint(0,2)
+    #computer_move = False
+ 
+    #if computer_move == False:
+	#if all_positions[posy][posx] == 0:
+	    #all_positions[posy][posx] = 1
+	    #computer_move = True
+    #else:
+	#randomComputerAI(all_positions)
+
+    #if computer_move == True:
+         #drawComputer(posx, posy, 0)
+         #return computer_move
