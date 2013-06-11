206f8b574ff045444587f1e7417d68ea477aa50c
Herman <herman.rogers@gmail.com>
2013-06-11 11:35:17 +0100

    separateing game.py into larger classes

--- game.py
+++ game.py
@@ -1,5 +1,5 @@
-'''This is free software created by Boomer Rogers and distributed
-   under the GNU General Public License (2013)and is intended for
+'''This is free software created by Boomer Rogers, distributed
+   under the GNU General Public License (2013), and is intended for
    non-commercial education purposes.'''
 
 from lib.board_settings.boardsettings import *
@@ -7,20 +7,30 @@ from lib.minimax_AI.minimaxAI import *
 
 class gameStart(object):
 
+    pygame.init()
+    pygame.display.set_caption('Tic-Tac-War')
+
     def mainGame(self):
-        pygame.init()
-        pygame.display.set_caption('Tic-Tac-War')
         graphical_board.generateNewBoard(80)
-        return self.gameLoop()
+        return gameMainLoop().gameLoop()
+
+class gameMainLoop(object):
 
     def gameLoop(self):
         while True:
             player = 'X'
-            self.toggleMusic()
-            self.endOfGame()
-            self.playerMakeMove(player)
-            pygame.display.update()
-            window_set.clock.tick(30)
+            gameToggleMusic().toggleMusic()
+            gamePlayerInput().playerMakeMove(player)
+            gameCheckConditions().checkBoard()
+
+class gameCheckConditions(object):
+
+    def checkBoard(self):
+        gameEnd().endOfGame()
+        pygame.display.update()
+        window_set.clock.tick(30)
+
+class gamePlayerInput(object):
 
     def playerMakeMove(self, player):
         for event in pygame.event.get():
@@ -28,7 +38,7 @@ class gameStart(object):
                 load_data.soundEffect()
 
                 if load_data.resetFunction().collidepoint(graphical_board.mouseCoords()):
-                    self.dataReset()
+                    gameReset().dataReset()
 
                 elif load_data.exitFunction().collidepoint(graphical_board.mouseCoords()):
                     gameExit()
@@ -36,20 +46,32 @@ class gameStart(object):
             elif event.type == MOUSEBUTTONUP:
                 load_data.soundEffect()
 
-                if self.toggleMusic().collidepoint(graphical_board.mouseCoords()) and pygame.mixer.music.get_busy() == 0:
+                if gameToggleMusic().toggleMusic().collidepoint(graphical_board.mouseCoords()) and pygame.mixer.music.get_busy() == 0:
                     return load_data.getMusic()
 
-		if self.toggleMusic().collidepoint(graphical_board.mouseCoords()) and pygame.mixer.get_busy():
+		if gameToggleMusic().toggleMusic().collidepoint(graphical_board.mouseCoords()) and pygame.mixer.get_busy():
 		    pygame.mixer.music.stop()
 
                 player_move = graphical_board.movePosition()
                 if not player_move in board_data.trackMovesLeft():
                     continue
                 board_data.makeMove(player_move, player)
-                return self.computerMakeMove(player)
+                return gameComputerMove().computerMakeMove(player)
 
             elif event.type == QUIT or (event.type == KEYDOWN and event.key == K_ESCAPE):
                 gameExit()
+
+class gameEnd(object):
+    def endOfGame(self):
+        if board_data.boardComplete():
+            if board_data.winner() == 'O':
+                load_data.computerWin()
+                    
+            if board_data.winner() == None:
+                load_data.displayDraw()
+            return load_data.resetFunction() and load_data.exitFunction()
+
+class gameComputerMove(object):
 
     def computerMakeMove(self, player):
         pygame.display.update()
@@ -58,7 +80,9 @@ class gameStart(object):
         time.sleep(0.7)
         board_data.makeMove(makecomp_move, player)
         load_data.soundEffect()
-        return self.playerMakeMove(player)
+        return gamePlayerInput().playerMakeMove(player)
+
+class gameToggleMusic(object):
 
     def toggleMusic(self):
 	music_on, music_off = load_data.loadPng('musicon.png'), load_data.loadPng('musicoff.png')
@@ -70,14 +94,7 @@ class gameStart(object):
             music = window_set.surface.blit(music_off, toggle)
         return music
 
-    def endOfGame(self):
-        if board_data.boardComplete():
-            if board_data.winner() == 'O':
-                load_data.computerWin()
-                    
-            if board_data.winner() == None:
-                load_data.displayDraw()
-            return load_data.resetFunction() and load_data.exitFunction()
+class gameReset(object):
 
     def dataReset(self):
         board_data.all_positions = [0,0,0,0,0,0,0,0,0]
--- spec/game_test.py
+++ spec/game_test.py
@@ -7,13 +7,9 @@ from game import *
 class testGameStart(unittest.TestCase):
 
     def setUp(self):
-        self.main_game = game_start
-        self.game_data = board_data
-        self.player = 'X'
+    
 
-    def testEndOfGameComputerWin(self):
-        self.game_data.boardComplete() == False
-        self.assertEqual(None, self.main_game.endOfGame())
+
 
 if __name__ == '__main__':
     unittest.main()
